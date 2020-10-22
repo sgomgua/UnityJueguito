f7d0b949a5ee40a586d43ec5916a633c2b5bcdc9
santiGG <sgomgua@gmail.com>
2020-10-21 21:27:26 -0500

    cambios recuperacion git

---
+++ JuegoSemestre7/Assets/Scripts/Slenderman/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float movX, movY, jump;
+    private Vector2 forceVector;
+    public Animator anim;
+    public float maxSpeed = 1f;
+
+    public float forceMultiplier , jumpMultiplier;
+
+    private Rigidbody2D rb;
+
+    //jump
+    private bool enSuelo = true;
+    public Transform comrobarSuelo;
+    float comprobarRadio = 0.07f;
+    public LayerMask mascaraSuelo;
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movX = Input.GetAxis("Horizontal");
+        movY = Input.GetAxis("Vertical");
+        jump = Input.GetAxis("Jump");
+        anim.SetFloat("Speed_X",Mathf.Abs(movX) );
+        float limitedSpeed = Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed);
+        rb.velocity = new Vector2(limitedSpeed, rb.velocity.y);
+        if (movX < 0f)
+        {
+            transform.localScale = new Vector3(-1.7f, 1.7f, 1f);
+        }
+        else{
+            transform.localScale = new Vector3(1.7f, 1.7f, 1f);
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        forceVector = new Vector2(movX, 0f) * forceMultiplier;
+
+        rb.AddForce(forceVector, ForceMode2D.Force);
+
+        enSuelo = Physics2D.OverlapCircle(comrobarSuelo.position, comprobarRadio, mascaraSuelo);
+        //if (enSuelo)
+        //{
+        //    salto2 = false;
+        //}
+        if ((enSuelo) && jump > 0)
+        {
+            rb.AddForce(Vector2.up * jumpMultiplier, ForceMode2D.Impulse);
+            //if (!salto2 && !enSuelo)
+            //{
+            //    salto2 = true;
+            //}
+        }
+    }
+}
